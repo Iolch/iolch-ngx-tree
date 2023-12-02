b1eb42bfef1baae3abc14312cca5e9e7a137e239
Iolanda Chagas <iolanda.chagas@nibo.com.br>
2023-12-02 14:12:50 -0300

    cria modelo e simplifica componente tree

--- src/components/tree/model/tree.model.ts
+++ src/components/tree/model/tree.model.ts
@@ -1,3 +1,43 @@
+import { TemplateRef } from "@angular/core";
+
 export interface NgxTreeConfig<T> {
     nodes: T[];
 };
+
+export interface TreeLevel {
+    template: TemplateRef<any>;
+    property: string;
+}
+
+export class FlatTree {
+    private levels: TreeLevel[];
+    private _nodes: any[];
+
+    constructor(levels: TreeLevel[]) {
+        this.levels = levels;
+        this._nodes = [];
+    }
+
+    public set nodes(nodes: any[]) {
+        this.toFlatTree(nodes);
+    }
+
+    public get nodes(): any[] {
+        return this._nodes;
+    }
+    
+    private toFlatTree(nodes: any[], level = 0){
+        nodes.forEach((node: any) => this.addNode(node, level));
+    }
+    
+    private addNode(node: any, level = 0){
+        this._nodes.push({...node, template: this.levels[level].template, level});
+
+        const { property } = this.levels[level +1] ?? {};
+        const child = node[property];
+        
+        if(child){
+            this.toFlatTree(child, level + 1);
+        }
+    }
+}
--- src/components/tree/tree.component.ts
+++ src/components/tree/tree.component.ts
@@ -1,52 +1,28 @@
 import { Component, ContentChildren, Input, QueryList, TemplateRef, ViewChild } from '@angular/core';
 
-import { NgxTreeConfig } from './model/tree.model';
 import { NgxTreeNodeDirective } from './tree-node.directive';
-
-interface Level {
-  template: TemplateRef<any>;
-  property: string;
-}
+import { FlatTree, NgxTreeConfig, TreeLevel } from './model/tree.model';
 
 @Component({
   selector: 'ngx-tree',
   templateUrl: './tree.component.html'
 })
 export class NgxTreeComponent {
-  
-  protected treeLevels!: Level[];
-
-  protected flatTree: any[];
+  protected flatTree!: FlatTree;
 
   @Input() config!: NgxTreeConfig<any>;
 
   @ContentChildren(NgxTreeNodeDirective) set treeNode(treeNodes: QueryList<NgxTreeNodeDirective>) {
+    const levels: TreeLevel[] = [];
+    
     treeNodes.forEach((treeNode: NgxTreeNodeDirective, level) => {
-      this.treeLevels[level] = {
+      levels[level] = {
         template: treeNode.template,
         property: treeNode.property
       };
     });
 
-    this.toFlatTree(this.config.nodes);
-  }
-
-  constructor() {
-    this.flatTree = [];
-    this.treeLevels = [];
-  }
-
-  private toFlatTree(nodes: any[], level = 0){
-    nodes.forEach((node: any) => this.checkNode(node, level));
-  }
-
-  private checkNode(node: any, level = 0){
-    this.flatTree.push({...node, template: this.treeLevels[level].template, level});
-
-    const { property } = this.treeLevels[level +1] ?? {};
-    const child = node[property];
-    if(child){
-      this.toFlatTree(child, level + 1);
-    }
-  }
+    this.flatTree = new FlatTree(levels);
+    this.flatTree.nodes = this.config.nodes;
+  }  
 }
